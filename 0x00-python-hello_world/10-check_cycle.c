2c83b1ec4e3b7b16d683b5526123b8de6b16cdbc
valvarezgi <vale.agiraldo@gmail.com>
2020-04-27 17:29:59 -0500

    Task 10: Technical interview preparation

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * check_cycle - checks for a cycle in a linked list
+ * @list : linked list to check
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *traverse;
+
+	if (list && list->next)
+		traverse = list->next;
+	else
+		return (0);
+
+	while (list && traverse)
+	{
+		if (list == traverse)
+			return (1);
+
+		list = list->next;
+
+		if (traverse->next)
+			traverse = traverse->next;
+
+		else
+			return (0);
+	}
+	return (0);
+}
