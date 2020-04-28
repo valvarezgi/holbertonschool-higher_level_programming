8fcf1bb82c075ca629c6cee855fc2ff860cc0dd7
valvarezgi <vale.agiraldo@gmail.com>
2020-04-28 15:40:50 -0500

    PEP8 issues fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for tens in range(0, 9):
     for units in range(tens + 1, 10):
         if tens == 8 and units == 9:
-            print("{}{}".format(tens, units))
+            print("{:d}{:d}".format(tens, units))
         else:
             print("{:d}{:d},".format(tens, units), end="")
