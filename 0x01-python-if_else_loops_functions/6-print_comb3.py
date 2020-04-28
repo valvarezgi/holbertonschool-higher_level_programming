da41d12bb008582ddfdd2a8f762e15bce410db80
valvarezgi <vale.agiraldo@gmail.com>
2020-04-28 15:33:48 -0500

    Task 6 added, first version

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for tens in range(0, 9):
+    for units in range(tens + 1, 10):
+        if tens == 8 and units == 9:
+            print("{}{}".format(tens, units))
+        else:
+            print("{}{},".format(tens, units), end="")
+            
