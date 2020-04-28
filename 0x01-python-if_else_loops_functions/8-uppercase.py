a09de0003d07d230ce3671332cdf62507c80fb3d
valvarezgi <vale.agiraldo@gmail.com>
2020-04-28 16:13:30 -0500

    Task 8 added, first version

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for character in str:
+        if ord(character) >= 97 and ord(character) <= 122:
+            character = chr(ord(character) - 32)
+        print("{:s}".format(character), end="")
+    print()
