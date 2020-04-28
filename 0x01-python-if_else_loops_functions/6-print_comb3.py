ba8d965f955d86d2bdbeb7f07430187141e819b1
valvarezgi <vale.agiraldo@gmail.com>
2020-04-28 15:43:00 -0500

    Added missing white space btween numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for tens in range(0, 9):
         if tens == 8 and units == 9:
             print("{:d}{:d}".format(tens, units))
         else:
-            print("{:d}{:d},".format(tens, units), end="")
+            print("{:d}{:d}, ".format(tens, units), end="")
