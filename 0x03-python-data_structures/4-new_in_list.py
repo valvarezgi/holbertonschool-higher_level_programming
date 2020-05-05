7409f8e540681e7f26791bcd51884b79307b2c8b
valvarezgi <vale.agiraldo@gmail.com>
2020-05-05 16:37:24 -0500

    Changed if for elif

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     new_list = list(my_list)
     if idx < 0:
         return new_in_list
-    if idx >= len(my_list):
+    elif idx >= len(my_list):
         return new_list
     else:
         new_list[idx] = element
