9c37abbda567cd6015cbb40b377fa71bfa75b652
valvarezgi <vale.agiraldo@gmail.com>
2020-05-05 15:56:24 -0500

    Task 1, logic changed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if idx >= len(my_list):
+    elif idx >= len(my_list):
         return None
-    return my_list[idx]
+    else:
+        return my_list[idx]
