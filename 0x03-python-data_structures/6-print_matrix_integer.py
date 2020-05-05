96f17c482e024e445cc406f376b318765c80d435
valvarezgi <vale.agiraldo@gmail.com>
2020-05-05 17:27:24 -0500

    Task 6, first version added

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        for y in x:
+            if y is x[-1]:
+                print("{:d}".format(y), end="")
+            else:
+                print("{:d}".format(y), end=" ")
+        print("")
