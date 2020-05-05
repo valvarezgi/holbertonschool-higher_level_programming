0b304a4a7201d683b6d8f3d6e7c45e0c55da4f13
valvarezgi <vale.agiraldo@gmail.com>
2020-05-05 16:44:11 -0500

    Wrong name in variable to return changed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     new_list = list(my_list)
     if idx < 0:
-        return new_in_list
+        return new_list
     elif idx >= len(my_list):
         return new_list
     else:
