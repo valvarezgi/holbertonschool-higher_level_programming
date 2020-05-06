093e567af9cd91d3812fa32dae77d48caa82657d
valvarezgi <vale.agiraldo@gmail.com>
2020-05-05 19:04:50 -0500

    Task 8 added, handlen if sentence is empty case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return len(sentence), sentence[0]
+    if not sentence:
+        return len(sentence), None
+    else:
+        return len(sentence), sentence[0]
