d6772e7a9a2c7b3aac6331eb3d879681e6ab3b55
valvarezgi <vale.agiraldo@gmail.com>
2020-05-07 20:46:58 -0500

    Task 12: Roman to Integer. Style issues fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,15 +2,14 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
-    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 
-                'D': 500, 'M': 1000}
+    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     integer = 0
 
-    for element in range(0, len(roman_string)):
-        if element == len(roman_string) - 1:
-            integer += values[roman_string[element]]
-        elif values[roman_string[element]] >= values[roman_string[element + 1]]:
-            integer += values[roman_string[element]]
+    for elem in range(0, len(roman_string)):
+        if elem == len(roman_string) - 1:
+            integer += values[roman_string[elem]]
+        elif values[roman_string[elem]] >= values[roman_string[elem + 1]]:
+            integer += values[roman_string[elem]]
         else:
-            integer -= values[roman_string[element]]
+            integer -= values[roman_string[elem]]
     return integer
