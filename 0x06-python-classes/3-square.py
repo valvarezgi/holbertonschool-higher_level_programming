81443ef1ecf444cf8e3642f8b3a12d2673ffb4bf
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-05-19 18:11:14 -0500

    [Feature] Task 3: added public instance method 'area'

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Square class that defines a square"""
+
+
+class Square:
+    """Defines Square Class.
+
+    Square class with private attribute size
+    """
+    def __init__(self, size=0):
+        """__init__ size
+
+        Args:
+            size (int, optional): Size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """area method
+
+        Area of the Square
+
+        Returns:
+            integer: square area
+        """
+        return self.__size * self.__size
