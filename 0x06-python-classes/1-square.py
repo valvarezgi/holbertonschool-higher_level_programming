73fae8a5e94d7e5c975cecf87565e55a38e95da1
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-05-19 17:13:33 -0500

    [Feature] Task 1: Write a class Square that defines a square instance by size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square class with private attribute size"""
+
+
+class Square:
+    """Defines Square Class.
+
+    Square class with private attribute size
+    """
+    def __init__(self, size):
+        """__init__ size
+
+        Args:
+            size (integer): private attribute for size
+        """
+        self.__size = size
