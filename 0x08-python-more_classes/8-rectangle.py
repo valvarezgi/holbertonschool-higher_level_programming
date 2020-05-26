b15174d28acd33f4c7ef3bdb410f9756792bb01d
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-05-25 20:31:09 -0500

    [Fix] Task 8:

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -143,5 +143,5 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        else: 
+        else:
             rect_2
