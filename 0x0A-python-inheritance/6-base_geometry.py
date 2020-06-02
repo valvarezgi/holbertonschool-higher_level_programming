2026547878ff8b1c29dc4684f69f12dec1dd3037
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-06-02 18:46:12 -0500

    [Feature] Task 6:

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ class with a public attribute
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
