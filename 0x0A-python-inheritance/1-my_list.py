92609e73e8b20c2db1e5648a535bb3224273ea32
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-06-02 18:12:29 -0500

    [Feature] Task 1: Class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" MyList
+"""
+
+
+class MyList(list):
+    """MyList
+
+    Args:
+        list (class): Inherits from list
+    """
+
+    def print_sorted(self):
+        """print_sorted sorted a given list
+        """
+        print(sorted(self))
