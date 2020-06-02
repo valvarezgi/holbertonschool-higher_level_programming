7c1b7862c64c22dba1e18c871dd3f1d89bb78108
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-06-02 18:17:41 -0500

    [Feature] Task 2: Function that evaluates if an object is exactly a given instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """is_same_class
+
+    Args:
+        obj (object): object to evaluate
+        a_class (class): class
+
+    Returns:
+        Bool: True is the object is exactly an istance of the
+        a_class
+    """
+    return (type(obj) is a_class)
