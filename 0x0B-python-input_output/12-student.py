3497e24315d0e954a817369988943f1875af6f4f
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-06-03 17:56:29 -0500

    [Feature] Task 12: Student to JSON with filter

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Class Student
+"""
+
+
+class Student:
+    """ Class Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """__init__
+
+        Args:
+            first_name (str): first name of the student
+            last_name (str): last name of the student
+            age (int): age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json
+
+        Returns:
+            [type]: [description]
+        """
+        if attrs is not None:
+            dictionary = {}
+            for key_1, value in self.__dict__.items():
+                for key_2 in attrs:
+                    if key_1 == key_2:
+                        dictionary.update({key_1: value})
+            return dictionary
+        else:
+            return self.__dict__
