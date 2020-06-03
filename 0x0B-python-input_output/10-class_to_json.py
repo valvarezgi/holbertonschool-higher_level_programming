9ea58e8e00d068e3f513b52ef9357036cbf82c38
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-06-03 17:20:14 -0500

    [Fix] Style issues fixed

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """ class_to_json
 """
+
+
 def class_to_json(obj):
-    """class_to_json 
+    """class_to_json
 
     Args:
-        obj (Any): 
+        obj (Any):
 
     Returns:
-        any : dictionary description 
+        any : dictionary description
     """
     return obj.__dict__
