5a11980308e847a75bd5b7b4739fdea4e6905f51
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-06-03 17:17:29 -0500

    [Feature] Task 10: Class to JSON

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class_to_json
+"""
+def class_to_json(obj):
+    """class_to_json 
+
+    Args:
+        obj (Any): 
+
+    Returns:
+        any : dictionary description 
+    """
+    return obj.__dict__
