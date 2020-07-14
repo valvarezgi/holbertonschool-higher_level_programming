b41c5cfc8b0bad0c655101c37d076850a9db32a6
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-07-14 17:11:59 -0500

    [Fix] Changed tittle for title

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 --  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title, tv_shows_genres.genre_id FROM tv_shows, tv_shows_genres
-WHERE tv_shows.id = tv_shows_genres.show_id ORDER BY tittle, genre_id;
+WHERE tv_shows.id = tv_shows_genres.show_id ORDER BY title, genre_id;
