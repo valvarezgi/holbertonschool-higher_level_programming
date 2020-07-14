1501dd576feefc2dbe3fcdf849df9fbb01f868d2
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-07-14 16:06:27 -0500

    [Feature] Task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Create database and table.
+-- Check if database 'hbtn_0d_usa' exists and if not, create it.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
+    (id INT PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (state_id) 
+            REFERENCES states(id),
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL
+);
