dca9a4daa3df3da43dae86fa41a41a73b778e738
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-08 11:20:22 -0500

    [Feature] Task 2: Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states  WHERE name LIKE '{}'".format(argv[4]))
+    cur.execute("SELECT * FROM states  WHERE name LIKE BINARY '{}'".format(argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
