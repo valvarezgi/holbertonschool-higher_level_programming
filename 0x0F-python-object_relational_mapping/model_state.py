e1b9925117086c6f851e0960a65ec1c2073ae713
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-08 14:24:02 -0500

    [Feature] Task 6: First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""SQLAlchemy"""
+""" State class module"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,8 +8,9 @@ Base = declarative_base()
 
 
 class State(Base):
-    """MySQL states table"""
-    __tablename___ = 'states'
+    """ MySQL states table representation
+    """
+    __tablename__ = 'states'
 
-    id = Column(Integer, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    name = Column(String(length=128), nullable=False)
