a587a301b06eaa2a8ba97466887b2cf160c86371
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-09 09:59:15 -0500

    [Feature] Task 13: Delete states

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""List all State objects"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    fil = session.query(State).filter(State.name.like('%a%'))
+    for filt in fil:
+        fil.delete(filt)
+        session.flush()
+        session.commit()
+        session.close()
+    session.close()
