642a2074c75f03e03ba22426259e8854a0179436
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-09 09:31:38 -0500

    [Feature] Task 10: Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""List all State objects"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    selections = session.query(State).order_by(State.id).all()
+    for selection in selections:
+        if selection.name == argv[4]:
+            print(selection.id)
+            session.close()
+            exit()
+    print("Not found")        
+    session.close()
