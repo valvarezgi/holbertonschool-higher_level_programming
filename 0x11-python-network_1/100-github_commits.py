eaef1fc2ef6d3a47d622a6e62468a96dd0f6fdc6
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-24 18:37:09 -0500

    Style issues fixed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,9 +4,9 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    Link = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    ReqData = requests.get(Link)
-    commits = ReqData.json()
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
         print(commit.get('commit').get('author').get('name'))
