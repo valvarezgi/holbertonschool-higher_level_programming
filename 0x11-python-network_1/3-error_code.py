e706d35b1a1c9bddc08a16e37a6c1ae514e309aa
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-24 13:39:44 -0500

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""displays the body of a url response decoded in utf-8"""
+from urllib import error, request
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read().decode())
+        except error.HTTPError as http_error:
+            print('Error code:{}'.format(http_error.code))
