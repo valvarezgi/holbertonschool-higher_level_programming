862a36478e9412275687315f5df883cd2e053106
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-09-15 19:22:03 -0500

    Task 10: Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function factorial (number) {
+  if (number === 1 || isNaN(number)) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
+console.log(factorial(parseInt(process.argv[2])));
